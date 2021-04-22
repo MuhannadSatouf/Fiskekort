09d99556f42753c08fe95508cf9bd02997bc21b3
MuhannadSatouf <57774753+MuhannadSatouf@users.noreply.github.com>
2021-04-22 16:54:52 +0200

    add new user to the realtime database, User class

--- app/src/main/java/com/example/fiskekort/Register.java
+++ app/src/main/java/com/example/fiskekort/Register.java
@@ -3,6 +3,10 @@ package com.example.fiskekort;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -12,10 +16,17 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.fiskekort.DB.DBMethod;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class Register extends AppCompatActivity implements View.OnClickListener {
     private EditText editTextfullname, editTextEmail, editTextPassword, editTextPhone;
@@ -23,6 +34,7 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
     private TextView returnToLogin;
     private FirebaseAuth mAuth;
     private ProgressBar progressBar;
+    private DBMethod dbMethod;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +44,7 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
         editTextfullname = findViewById(R.id.textName);
         editTextEmail = findViewById(R.id.textEmail);
         editTextPassword = findViewById(R.id.passWordText);
+        editTextPhone = findViewById(R.id.phoneNr);
         registerButton = findViewById(R.id.registerButton);
         registerButton.setOnClickListener(this);
         returnToLogin = findViewById(R.id.textReturnToLogin);
@@ -56,9 +69,10 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
     private void registerUser() {
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
-        String playerName = editTextfullname.getText().toString().trim();
+        String name = editTextfullname.getText().toString().trim();
+        String phoneNumber = editTextPhone.getText().toString().trim();
 
-        if (playerName.isEmpty()) {
+        if (name.isEmpty()) {
             editTextfullname.setError("please provide you full name");
             editTextfullname.requestFocus();
             return;
@@ -92,22 +106,47 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
 
         progressBar.setVisibility(View.VISIBLE);
         mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(Register.this, "User has been register successfully", Toast.LENGTH_LONG).show();
-                            finish();
-                            progressBar.setVisibility(View.INVISIBLE);
-                        }else {
-                            Toast.makeText(Register.this,"Failed to register try again!",Toast.LENGTH_LONG).show();
-                            progressBar.setVisibility(View.INVISIBLE);
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+
+                        if (fUser != null) {
+                            User user = new User(name, email, password, phoneNumber);
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(fUser.getUid())
+                                    .setValue(user).addOnCompleteListener(task1 -> {
+                                if (task1.isSuccessful()) {
+                                    createToast("User has been registered successfully", Register.this);
+                                    finish();
+                                    progressBar.setVisibility(View.INVISIBLE);
+                                } else {
+                                    createToast("Failed to register! Try again", Register.this);
+                                }
+                            });
+                        }
+                    } else {
+                        try {
+                            throw Objects.requireNonNull(task.getException());
+                        } catch (FirebaseAuthInvalidCredentialsException malformedEmail) {
+                            createToast("Wrong Password! Try again", Register.this);
+
+                        } catch (FirebaseAuthUserCollisionException existEmail) {
+
+                            createToast("This Email already exists", Register.this);
+
+
+                        } catch (Exception e) {
+                            createToast("Failed to register! Try again", Register.this);
+
+                        }
+
                     }
-                }
-    });
-}
+                });
+    }
 
-
+    public void createToast(String text, Context context) {
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+    }
 }
 
 
