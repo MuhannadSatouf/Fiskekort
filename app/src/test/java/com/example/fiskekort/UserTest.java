a153d1cf0c059de772dbe63c2627b34e17fdbc56
liliia <liliia.allansson0085@stud.hkr.se>
2021-04-27 13:27:15 +0200

    gradle changed, UserTest finalized

--- app/src/test/java/com/example/fiskekort/UserTest.java
+++ app/src/test/java/com/example/fiskekort/UserTest.java
@@ -27,21 +27,31 @@ public class UserTest {
 
     @Test
     public void setEmail() {
+        testUser.setEmail("MyName@gmail.com");
+        assertEquals("MyName@gmail.com", testUser.getEmail());
     }
 
     @Test
     public void getPassword() {
+        testUser.setPassword("Mypass");
+        assertEquals("Mypass", testUser.getPassword());
     }
 
     @Test
     public void setPassword() {
+        testUser.setPassword("Mypass");
+        assertEquals("Mypass", testUser.getPassword());
     }
 
     @Test
     public void getPhoneNumber() {
+        testUser.setPhoneNumber("07623586145");
+        assertEquals("07623586145", testUser.getPhoneNumber());
     }
 
     @Test
     public void setPhoneNumber() {
+        testUser.setPhoneNumber("07623586145");
+        assertEquals("07623586145", testUser.getPhoneNumber());
     }
 }
