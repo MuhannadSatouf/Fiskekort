461a438f69966759c180a3a3e561b74071ef362c
liliia <liliia.allansson0085@stud.hkr.se>
2021-04-27 14:04:33 +0200

    a failed test test (Github Actions test)

---
+++ app/src/test/java/com/example/fiskekort/FishingCardTest.java
@@ -0,0 +1,18 @@
+package com.example.fiskekort;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FishingCardTest {
+
+    @Test
+    public void getOwner() {
+        assertEquals("owner", null);  //deliberately failed test
+    }
+
+    @Test
+    public void setOwner() {
+        
+    }
+}
