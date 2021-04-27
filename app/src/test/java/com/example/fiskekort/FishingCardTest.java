1fb63e718abd258568a8cb73c33b4fa04d5a1c0f
liliia <liliia.allansson0085@stud.hkr.se>
2021-04-27 14:16:14 +0200

    final test of TDD setup

--- app/src/test/java/com/example/fiskekort/FishingCardTest.java
+++ app/src/test/java/com/example/fiskekort/FishingCardTest.java
@@ -5,14 +5,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FishingCardTest {
+    User testUser = new User( "MyUserName", "myEmail@gmail.com", "pa$$", "0762357845");
+    FishingCard fc = new FishingCard(testUser, "12345", "today", "tomorrow");
 
     @Test
     public void getOwner() {
-        assertEquals("owner", null);  //deliberately failed test
+        assertEquals(testUser, fc.getOwner());
     }
 
     @Test
     public void setOwner() {
-        
+        User testUser2 = new User();
+        fc.setOwner(testUser2);
+        assertEquals(testUser2, fc.getOwner());
     }
 }
