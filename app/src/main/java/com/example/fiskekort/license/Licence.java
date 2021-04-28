103c3456e9651be5588924424ad3cd8c2958b87e
Deniel <Raghudeep.bathula98@gmail.com>
2021-04-28 22:17:39 +0200

    Copied files from main branch to master branch

---
+++ app/src/main/java/com/example/fiskekort/license/Licence.java
@@ -0,0 +1,27 @@
+package com.example.fiskekort.license;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.fiskekort.R;
+
+public class Licence extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_licence);
+
+        handleFragment(LicenceFragment.newInstance());
+    }
+
+    private void handleFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+        transaction.replace(R.id.licence_frameLayout, fragment);
+        transaction.commit();
+    }
+}
