68f986935a8fdf6202c8ed05969cc8d5d7c5b24d
liliia <liliia.allansson0085@stud.hkr.se>
2021-05-16 16:33:13 +0200

    added strings and removed unused tests

--- app/src/test/java/com/example/fiskekort/FishingCardTest.java
+++ app/src/test/java/com/example/fiskekort/FishingCardTest.java
@@ -5,22 +5,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FishingCardTest {
-    User testUser = new User( "MyUserName", "myEmail@gmail.com", "pa$$", "0762357845");
-    FishingCard fc = new FishingCard(testUser, "12345", "today", "tomorrow");
+  //  User testUser = new User( "MyUserName", "myEmail@gmail.com", "pa$$", "0762357845");
+    FishingCard fc = new FishingCard("12345", "today", "tomorrow");
 
-    @Test
-    public void getOwner() {
-
-        assertEquals(testUser,fc.getOwner());
-    }
-
-    @Test
-    public void setOwner() {
-        System.out.println("Unit Tests are being run");
-        User testUser2 = new User();
-        fc.setOwner(testUser2);
-        assertEquals(testUser2, fc.getOwner());
-    }
     @Test
     public void setCardNumber(){
         fc.setCardNumber("54321");
