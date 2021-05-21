e45284aee4ff125b3c027b5d707401475a5b6859
MuhannadSatouf <57774753+MuhannadSatouf@users.noreply.github.com>
2021-05-21 14:08:55 +0200

    Fix cnflict in Test classes

--- app/src/test/java/com/example/fiskekort/FishingCardTest.java
+++ app/src/test/java/com/example/fiskekort/FishingCardTest.java
@@ -5,22 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FishingCardTest {
-<<<<<<< HEAD
-    User testUser = new User( "MyUserName", "myEmail@gmail.com", "pa$$", "0762357845");
-    FishingCard fc = new FishingCard(testUser, "12345", "today", "tomorrow");
-
-    @Test
-    public void getOwner() {
-        assertEquals(testUser, fc.getOwner());
-    }
-
-    @Test
-    public void setOwner() {
-        User testUser2 = new User();
-        fc.setOwner(testUser2);
-        assertEquals(testUser2, fc.getOwner());
-    }
-=======
     FishingCard fc = new FishingCard("12345", "today", "tomorrow");
     private final Municipality mun = new Municipality("testMun");
     FishingCard fc2 = new FishingCard("2021-7-12", "2021-8-13", LocationType.MUNICIPALITY, mun);
@@ -85,5 +69,4 @@ public class FishingCardTest {
     }
 
 
->>>>>>> dev
 }
